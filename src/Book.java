f483c40eeeafc548fe1091925338494b7fece974
salem ALmotiry <60990492+salemALmotiry@users.noreply.github.com>
2024-11-06 13:57:31 +0300

    Initial commit
    Initial commit

--- src/Book.java
+++ src/Book.java
@@ -1,1 +1,28 @@
+public class Book extends Product {
+    private String Author;
+
+    @Override
+    public double getDiscount() {
+        return super.getPrice() * 0.6;
+    }
+
+
+    public Book (String name, double price, String director) {
+        super(name, price);
+        this.Author = director;
+    }
+
+    public Book() {
+
+    }
+
+
+    public String getAuthor() {
+        return Author;
+    }
+
+    public void setAuthor(String Author) {
+        this.Author = Author;
+    }
+
 }
